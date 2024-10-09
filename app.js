1a6dd07f4c606779bb310aead3f342ac2cf2d422
JheX <nicorodriguez242003@gmail.com>
2024-10-09 13:45:35 -0300

    added functionality to the button to operate with the buy value or sell value

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const fechasDOM = document.querySelectorAll('.fecha');
 const comprasDOM = document.querySelectorAll('.compra');
 const ventasDOM = document.querySelectorAll('.venta');
 const formsDOM = document.querySelectorAll('.compra-dolar');
+const toggleButtonsDOM = document.querySelectorAll('.checkbox');
 
 class Dolar {
-    constructor(fechaDOM, compraDOM, ventaDOM, formDOM) {
+    constructor(fechaDOM, compraDOM, ventaDOM, formDOM, toggleButtonDOM) {
         this.fecha = fechaDOM;
         this.compraDOM = compraDOM;
         this.compra = 0;
         this.ventaDOM = ventaDOM;
         this.venta = 0;
+        this.currentOperation = "";
         this.formUSD = formDOM.querySelector('[name="usd"]');
         this.formARS = formDOM.querySelector('[name="ars"]');
+        this.toggleButton = toggleButtonDOM;
     }
 
     #formatearPrecio(precio = 0) {
@@ -19,22 +22,23 @@ class Dolar {
     }
 
     render(dolar) {
-        this.fecha.textContent = new Date(dolar.fechaActualizacion).toLocaleString('es-ar', { minute: '2-digit', hour: '2-digit', day: '2-digit', month: 'short' });
+        this.fecha.textContent = getTimeDifference(new Date(dolar.fechaActualizacion)) + ` (${new Date(dolar.fechaActualizacion).toLocaleString('es-ar', { minute: '2-digit', hour: '2-digit', day: '2-digit', month: 'short' })})`;
         this.compraDOM.textContent = this.#formatearPrecio(dolar.compra);
         this.compra = dolar.compra;
         this.ventaDOM.textContent = this.#formatearPrecio(dolar.venta);
         this.venta = dolar.venta;
+        this.currentOperation = "compra";
         this.formARS.placeholder = dolar.compra.toLocaleString('es-AR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-        this.#loadFunctionality();
+        this.loadFunctionality();
     }
 
-    #loadFunctionality() {
-        this.formUSD.addEventListener('input', this.#convertCurrency.bind(this, "usd"));
-        this.formARS.addEventListener('input', this.#convertCurrency.bind(this, "ars"));
+    loadFunctionality() {
+        this.formUSD.addEventListener('input', this.convertCurrency.bind(this, "usd"));
+        this.formARS.addEventListener('input', this.convertCurrency.bind(this, "ars"));
+        this.toggleButton.addEventListener("click", this.toggleCurrency.bind(this));
     }
 
-    #convertCurrency(currency) {
-        // debugger;
+    convertCurrency(currency) {
         const isUSD = currency === "usd";
         const sourceInput = isUSD ? this.formUSD : this.formARS;
         const targetInput = isUSD ? this.formARS : this.formUSD;
@@ -46,24 +50,35 @@ class Dolar {
             return;
         }
 
-        const result = this.#convertResult(isUSD, value);
+        const result = this.convertResult(isUSD, value, this.currentOperation);
         targetInput.value = result;
     }
 
-    #convertResult(isUSD, value, min, max) {
-        return (isUSD ? value * this.compra : value / this.compra).toLocaleString('es-AR', { minimumFractionDigits: min || 2, maximumFractionDigits: !isUSD ? 5 : (max || 2) });
+    toggleCurrency() {
+        this.currentOperation = this.currentOperation === "compra" ? "venta" : "compra";
+        if (this.currentOperation === "compra")
+            this.formARS.placeholder = this.compra.toLocaleString('es-AR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        else if (this.currentOperation === "venta")
+            this.formARS.placeholder = this.venta.toLocaleString('es-AR', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+        this.formUSD.value = "";
+        this.formARS.value = "";
+    }
+
+    convertResult(isUSD, value, currentOperation, min, max) {
+        return (isUSD ? value * (currentOperation === "compra" ? this.compra : this.venta) : value / (currentOperation === "compra" ? this.compra : this.venta))
+            .toLocaleString('es-AR', { minimumFractionDigits: min || 2, maximumFractionDigits: !isUSD ? 5 : (max || 2) });
     }
 
 }
 
 function loadMap() {
-    if (!(fechasDOM.length === comprasDOM.length && comprasDOM.length === ventasDOM.length && ventasDOM.length === formsDOM.length)) {
+    if (!(fechasDOM.length === comprasDOM.length && comprasDOM.length === ventasDOM.length && ventasDOM.length === formsDOM.length && formsDOM.length === toggleButtonsDOM.length)) {
         console.error("Error: No se cargaron los elementos correctamente");
         return;
     }
     const dolarHouses = ["oficial", "blue", "bolsa", "cripto", "tarjeta"];
     const dolarMap = new Map();
-    dolarHouses.forEach((dolarHouse, index) => dolarMap.set(dolarHouse, new Dolar(fechasDOM[index], comprasDOM[index], ventasDOM[index], formsDOM[index])));
+    dolarHouses.forEach((dolarHouse, index) => dolarMap.set(dolarHouse, new Dolar(fechasDOM[index], comprasDOM[index], ventasDOM[index], formsDOM[index], toggleButtonsDOM[index])));
     return dolarMap;
 }
 
@@ -84,6 +99,18 @@ async function main() {
 
 function renderDolars(dolarList) {
     dolarList.forEach((dolar) => dolarMap.get(dolar.casa).render(dolar))
+}
+
+function getTimeDifference(date = new Date()) {
+    const difference = Date.now() - date;
+    const minutes = Math.floor(difference / 60000);
+    if (minutes < 60)
+        return `${minutes} minuto${minutes > 1 ? "s" : ""}`;
+    const hours = Math.floor(minutes / 60);
+    if (hours < 24)
+        return `${hours} hora${hours > 1 ? "s" : ""} y ${minutes - hours * 60} minutos`;
+    const days = Math.floor(hours / 24);
+    return `${days} día${days > 1 ? "s" : ""} y ${hours - days * 24} horas`;
 }
 
 
